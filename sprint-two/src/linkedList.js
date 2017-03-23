bb9bf33ac7806b171bd779eae092fb059e8fe932
aalcott14 <aalcott14@gmail.com>
2017-03-23 10:41:37 -0500

    Pass Tests in Linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,50 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  var count = 0;
+  var length = 0;
 
   list.addToTail = function(value) {
+    //if head is null link node to head
+    var currentNode = this.tail;
+    var newNode = Object.create(Node(value));
+    length++;
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      currentNode.next = newNode;
+      this.tail = newNode;      
+    }
+
+    //if not empty set tail node to new node and tail to new node
+
   };
 
   list.removeHead = function() {
+    //set head to current node's next node
+    if (this.head) {
+      var removeNode = this.head;
+      this.head = removeNode.next;
+    }
+    //return current node
+    length--;
+    return removeNode.value;
   };
 
   list.contains = function(target) {
+    //go through linked list by going through node links
+    //compare node to target and if found return true
+    currentNode = this.head;
+    while (count < length) {
+      debugger;
+      if (currentNode.value === target) {
+        return true;
+      }
+      count++;
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
